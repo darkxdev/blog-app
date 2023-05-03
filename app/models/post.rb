33105968f89f34c0602061237cc7e2b4e7e406cc
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-03 15:43:30 -0500

    Added custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def increment_comment_counter
+    update(comments_counter: comments_counter + 1)
+  end
+
+  def increment_like_counter
+    update(likes_counter: likes_counter + 1)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def last_three_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def increment_post_counter
+    update(post_counter: post_counter + 1)
+  end
 end

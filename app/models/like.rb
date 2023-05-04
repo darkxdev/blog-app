931710bef546e14ab1855705be1f57d700f6a2d7
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-04 15:56:03 -0500

    Added Validation and Model Specs

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-
-  def increment_like_counter
-    update(likes_counter: likes_counter + 1)
-  end
 end

dbd5d04b77427524106acf829545ea2c45d53a16
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-10 18:35:14 -0500

    Required changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:id/posts/:id', to: 'posts#show'
 
 end

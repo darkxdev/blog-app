97e08f4c553dc0a139dd9f3222f5a8f18d720c3b
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-11 17:14:59 -0500

    Added create new Posts and Comments funcionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:id/posts/:id', to: 'posts#show'
+  get '/users/:id/posts/new', to: 'posts#new', as: 'user_post_new'
+  post '/users/:id/posts/create', to: 'posts#create', as: 'user_post_create'
+  get '/users/:id/posts/:post_id', to: 'posts#show', as: 'post_comments'
+
+  post '/users/:id/posts/:post_id/create', to: 'comments#create', as: 'post_comments_create'
 
 end

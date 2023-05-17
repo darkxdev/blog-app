271ac5ecdab31fbf89147c85378baea3eca3c130
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-17 11:44:27 -0500

    Set up 'forgot password' devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: Rails.application.credentials.gmail[:username],
+  password: Rails.application.credentials.gmail[:password],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root 'users#index'
+
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/posts', to: 'posts#index', as: 'user_posts'

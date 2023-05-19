8cb1e38525d9c5b3303ec07655e2d100d705128f
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-19 16:15:27 -0500

    Removed unused code

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,12 +1,11 @@
-class Api::CommentsController < Api::MainController
-  skip_before_action :authenticate, only: :index
-
+class Api::CommentsController < ApplicationController
   def create
     auth_token = request.headers['Authorization']
     current_user = User.find_by(token: auth_token)
     @new_comment = Comment.new(params.require(:comment).permit(:text))
     @new_comment.author = current_user
     @new_comment.post = Post.find(params[:post_id])
+
     if @new_comment.save
       render json: @new_comment, status: :created
     else
